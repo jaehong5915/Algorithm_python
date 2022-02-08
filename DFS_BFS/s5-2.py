afd6e1f8904ff126c106bbbbb7ba25db86ab40b5
jaehong5915 <woghdclf@naver.com>
2022-02-09 00:01:22 +0900

    update s5-2

--- DFS_BFS/s5-2.py
+++ DFS_BFS/s5-2.py
@@ -1,28 +1,19 @@
-# 미로 탈출
+from collections import deque
+# 미로 탈출, 가까운 노드 -> 큐
 # 괴물 o -> o , 괴물 x -> 1
 ## 1. n,m / field 받기  2. 필드 벗어나면 False 3. 재귀 돌려서 괴물 없는 곳 탐색 
 
-n, m = map(int,input().split())
+n,m = map(int,input().split())
 field =[]
-for i in range(n):
-    field.append(list(map(int, input())))
+for _ in range(n):
+    field.append(map(int,input()))
 
-def dsx(x,y):
-    cnt =1
-    if x <= -1 or x >= n or y <= -1 or y >= m :
-        print('11111')
-        return False
-    
-    if field[x][y] == 1:
-        cnt += 1
-        # field[x][y] += 1
-        dsx(x,y+1)
-        dsx(x,y-1)
-        dsx(x-1,y)
-        dsx(x+1,y)
-        return cnt
-    return False
-for i in range(n):
-    for j in range(m):
-        result = dsx(i,j) 
-print(result)
+#이동 거리
+dx = [-1,1,0,0]
+dy = [0,0,-1,1]
+
+def bfs(x,y):
+    queue = deque()
+    queue.append((x,y))
+
+print(bfs(0,0))
