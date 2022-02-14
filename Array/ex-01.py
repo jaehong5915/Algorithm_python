d8a8a563bc2b9015270aa7661a98f97951c37ab2
jaehong5915 <woghdclf@naver.com>
2022-02-14 23:35:13 +0900

    Array/ex-o1

--- Array/ex-01.py
+++ Array/ex-01.py
@@ -7,5 +7,6 @@ n = int(input())
 data =[]
 for i in range(n):
     data.append(int(input()))
-data.sort()
-print(data)
+data.sort(reverse=True)
+for i in range(len(data)):
+    print(data[i], end=' ')
