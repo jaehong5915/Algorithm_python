a7e7f91f112c4083c1c663fa94bb507a37941561
jaehong5915 <woghdclf@naver.com>
2022-03-05 00:29:48 +0900

    s11055

--- DP/BAEKJOON/s11055.py
+++ DP/BAEKJOON/s11055.py
@@ -4,10 +4,10 @@
 
 n = int(input())
 data = list(map(int,input().split()))
-dp = [1] * (n+1)
-
+dp = [x for x in data]
 for i in range(n):
     for j in range(i):
-        if data[i] > data[j] :
-            dp[i] = max(dp[i], dp[j]+1)
-print(dp)
+        if data[i] > data[j]:
+            dp[i] = max(dp[i], dp[j] + data[i]) ##음수
+            # dp[i] = dp[j] + data[i]
+print(max(dp))
