36203a1c84c048baec48efb804b0a462530aec0a
jaehong5915 <woghdclf@naver.com>
2022-03-23 17:13:05 +0900

    Greedy Review

--- Greedy/s3.py
+++ Greedy/s3.py
@@ -1,8 +1,21 @@
-n,m = map(int, input().split())
+'''
+숫자 카드 게임
+- 행 중에 최소 값 뽑기 
+- 뽑은 값 중 가장 큰 값 
+'''
+n, m = map(int,input().split())
+data = []
+rs =[]
+for _ in range(n):
+    data=list(map(int,input().split()))
+    rs.append(min(data))
+print(max(rs))
+    
+# n,m = map(int, input().split())
 
-result = []
-for i in range(n):
-    data = list(map(int, input().split()))
-    min_value = min(data)
-    result.append(min_value)
-print(max(result))
+# result = []
+# for i in range(n):
+#     data = list(map(int, input().split()))
+#     min_value = min(data)
+#     result.append(min_value)
+# print(max(result))
--- Greedy/s4.py
+++ Greedy/s4.py
@@ -2,14 +2,29 @@
 ## N에서 1을 뺀다.  / N을 k로 나눈다.(N이 K로 나누어떨어질 때만 선택 가능.)
 ### 우선순위 나누는 것 (나누어 떨어질 때까지 1번 수행)
 
-n,k = map(int, input().split())
-count = 0
+# n,k = map(int, input().split())
+# count = 0
+# while True:
+#     if n % k ==0:
+#         n //= k
+#         count +=1;
+#     else:
+#         n -= 1    
+#         count +=1
+#     if n == 1 : break
+# print(count)
+
+
+n, k = map(int,input().split())
+cnt = 0
 while True:
-    if n % k ==0:
-        n //= k
-        count +=1;
+    if n == 1:
+        break
+    if n % k == 0:
+        n /= k
+        cnt += 1
     else:
-        n -= 1    
-        count +=1
-    if n == 1 : break
-print(count)
+        n -= 1
+        cnt += 1
+print(cnt)
+
