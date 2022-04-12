cb52f21a857d4f815c07efeb19ce86241a682b03
jaehong5915 <woghdclf@naver.com>
2022-04-12 23:55:00 +0900

    dfs - s1260

--- DFS_BFS/BAEKJOON/s1260_pra.py
+++ DFS_BFS/BAEKJOON/s1260_pra.py
@@ -5,39 +5,88 @@
 2. bfs, dfs -> 
 '''
 from collections import deque
-import sys
-input = sys.stdin.readline
+n,m,v = map(int,input().split())
+graph = [[0]* (n+1) for _ in range(n+1)]
 
-n, m, v = map(int, input().split())
-graph = [[0]*(n+1) for _ in range(n+1)]
-# visited = [False]*(n+1) #visited 하나로 가능한지?
-# visited1 = [False]*(n+1)
-#양방향 그래프
 for _ in range(m):
-    a,b = map(int,input().split())
+    a, b = map(int,input().split())
     graph[a][b] = 1
     graph[b][a] = 1
-
+# def dfs(v):
+#     visited[v] = True
+#     print(v, end=' ')
+#     for i in range(1,n+1):
+#         if visited[i] == False and graph[v][i] == 1:
+#             dfs(i)
 def dfs(v):
-    visited[v] = True
-    print(v, end=' ')
+    visit[v] == True
+    print(v,end=' ')
     for i in range(1,n+1):
-        if visited[i] == False and graph[v][i] == 1:
+        if graph[v][i] == 1 and visit[i] == False:
             dfs(i)
-
 def bfs(v):
-    visited[v] = True
+    visit[v] = True
     q = deque()
     q.append(v)
     while q:
-        v = q.popleft()
-        print(v, end=' ')
+        k = q.popleft()
+        print(k, end=' ')
         for i in range(1, n+1):
-            if visited[i] == False and graph[v][i] == 1:
+            if visit[i] == False and graph[k][i] == 1:
+                visit[i] = True
                 q.append(i)
-                visited[i] = True
-visited = [False]*(n+1)                
+visit=[False] * (n+1)
 dfs(v)
-visited = [False]*(n+1)
 print()
+visit=[False] * (n+1)
 bfs(v)
+
+
+
+
+
+
+
+
+
+
+
+
+
+# from collections import deque
+# import sys
+# input = sys.stdin.readline
+
+# n, m, v = map(int, input().split())
+# graph = [[0]*(n+1) for _ in range(n+1)]
+# # visited = [False]*(n+1) #visited 하나로 가능한지?
+# # visited1 = [False]*(n+1)
+# #양방향 그래프
+# for _ in range(m):
+#     a,b = map(int,input().split())
+#     graph[a][b] = 1
+#     graph[b][a] = 1
+
+# def dfs(v):
+#     visited[v] = True
+#     print(v, end=' ')
+#     for i in range(1,n+1):
+#         if visited[i] == False and graph[v][i] == 1:
+#             dfs(i)
+
+# def bfs(v):
+#     visited[v] = True
+#     q = deque()
+#     q.append(v)
+#     while q:
+#         v = q.popleft()
+#         print(v, end=' ')
+#         for i in range(1, n+1):
+#             if visited[i] == False and graph[v][i] == 1:
+#                 q.append(i)
+#                 visited[i] = True
+# visited = [False]*(n+1)                
+# dfs(v)
+# visited = [False]*(n+1)
+# print()
+# bfs(v)
