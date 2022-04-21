54a0d636df0ffef52fb1606ad2b59e349f5caf8f
jaehong5915 <woghdclf@naver.com>
2022-04-21 23:57:06 +0900

    update README

--- Implementation/BAEKJOON/s1475.py
+++ Implementation/BAEKJOON/s1475.py
@@ -4,20 +4,17 @@
 
 다시 풀어보기 꼭
 '''
+
+
 n = input()
-
-count = {'0':0, '1':0, '2':0, '3':0, 
-'4':0, '5':0, '6':0, '7':0, '8':0}
-
-for i in range(len(n)) :
-    if n[i] in ['6', '9'] :
-        count['6'] += 1
-    else :
-        count[n[i]] += 1
-
-if count['6'] % 2 == 0:
-  count['6'] = count['6'] // 2
-else :
-  count['6'] = count['6'] // 2 + 1
-print(count.values())
-print(max(count.values()))
+rs = {'0':0, '1':0, '2':0,'3':0,'4':0,'5':0,'6':0,'7':0,'8':0}
+for i in range(len(n)):
+    if n[i] in ['6','9']:
+        rs['6'] += 1
+    else:
+        rs[n[i]] += 1
+if rs['6'] %2 == 0:
+    rs['6'] = rs['6']//2
+else:
+    rs['6'] = rs['6']//2 + 1
+print(max(rs.values()))
