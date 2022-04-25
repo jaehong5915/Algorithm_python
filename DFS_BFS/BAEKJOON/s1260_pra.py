9f41e5b42013025b32651fcdc600ac7ed464310a
jaehong5915 <woghdclf@naver.com>
2022-04-26 00:01:25 +0900

    s1260 re

--- DFS_BFS/BAEKJOON/s1260_pra.py
+++ DFS_BFS/BAEKJOON/s1260_pra.py
@@ -8,12 +8,11 @@ from collections import deque
 
 n,m,v = map(int,input().split())
 graph = [[0] * (n+1) for _ in range(n+1)]
-dfs_visit = [False] * (n+1)
-bfs_visit = [False] * (n+1)
+
 for _ in range(m):
     a, b = map(int,input().split())
-    graph[a][b] == 1
-    graph[b][a] == 1
+    graph[a][b] = 1
+    graph[b][a] = 1
 # v 시작 노드
 def dfs(v):
     dfs_visit[v] = True
@@ -33,7 +32,8 @@ def bfs(v):
             if bfs_visit[i] == False and graph[x][i] == 1:
                 bfs_visit[i] = True
                 q.append(i)
-
+dfs_visit = [False] * (n+1)
+bfs_visit = [False] * (n+1)
 dfs(v)
 print()
 bfs(v)
